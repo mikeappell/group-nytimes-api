e70ebe52b1f75ae992b6f240962f40038af347c7
JustinNazari <justinnazari@gmail.com>
2016-04-12 15:52:23 -0400

    user input now working

--- data_fetchers/api_communicator.rb
+++ data_fetchers/api_communicator.rb
@@ -4,14 +4,21 @@ require 'JSON'
 
 class APICommunicator
 
-attr_accessor :search_params, :raw_data, :parsed_data
+attr_accessor :search_params, :raw_data, :parsed_data, :user_query
   
-  def initialize(search_terms)
-    @search_params = search_terms
+  def initialize(user_query)
+    @user_query = user_query
+  end
+
+  def get_query_URI
+    movie_base = "http://api.nytimes.com/svc/movies/v2/reviews/search.json?query="
+    api_key = "api-key=0fa7004e9139a09e1423bd884a8947c9:18:62278142"
+    interpreted_string = user_query.tr(" ", "+")
+    self.search_params = movie_base + interpreted_string + "&" + api_key 
   end
 
   def get_JSON_file
-    self.raw_data = RestClient.get("http://api.nytimes.com/svc/movies/v2/reviews/search.json?query=big&thousand-best=Y&opening-date=1930-01-01;2000-01-01&api-key=0fa7004e9139a09e1423bd884a8947c9:18:62278142")
+    self.raw_data = RestClient.get(search_params)
   end
 
   def parse_JSON
@@ -19,6 +26,7 @@ attr_accessor :search_params, :raw_data, :parsed_data
   end
 
   def get_data
+    self.get_query_URI
     self.get_JSON_file
     self.parse_JSON
     results_array = parsed_data["results"].collect do |entry|
