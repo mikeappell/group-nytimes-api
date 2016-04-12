18ecfbcd77b2a928ba40409e6b694fef7ea9ad7c
JustinNazari <justinnazari@gmail.com>
2016-04-12 17:57:56 -0400

    reworked object orientation model and runner file

--- models/User.rb
+++ models/User.rb
@@ -1,9 +1,14 @@
 class User
   
-  attr_accessor :user_input, :movie_search_terms, :past_searches
+  attr_accessor :user_input, :movie_search_terms, :past_searches, :review
 
     def initialize
       @past_searches = []
+    end
+
+    def movie_search_terms=(terms)
+      @movie_search_terms = terms
+      past_searches << terms
     end
 
 end
