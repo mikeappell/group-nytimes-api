b71449da85eaee7079983061dce53de5643e519e
Michael Appell <mikeappell@gmail.com>
2016-04-12 16:15:32 -0400

    Tester work

--- data_fetchers/api_communicator.rb
+++ data_fetchers/api_communicator.rb
@@ -4,20 +4,27 @@ require 'JSON'
 
 class APICommunicator
 
-attr_accessor :search_params
+  attr_accessor :search_params, :apikey
   
   def initialize(search_terms)
-    @search_params = search_terms
+    @apikey = "0fa7004e9139a09e1423bd884a8947c9:18:62278142"
+    @search_params = "\'#{search_terms}\'"
   end
 
   def get_JSON_file
-    return_value = RestClient.get("http://api.nytimes.com/svc/movies/v2/reviews/search.json?query=big&thousand-best=Y&opening-date=1930-01-01;2000-01-01&api-key=0fa7004e9139a09e1423bd884a8947c9:18:62278142")
+    request = {:params => {:query => search_params, "api-key" => apikey}}
+    # return_value = RestClient.get("http://api.nytimes.com/svc/movies/v2/reviews/search.json", request)
+    return_value = RestClient.get("http://api.nytimes.com/svc/movies/v2/reviews/search.json?query=big&api-key=0fa7004e9139a09e1423bd884a8947c9:18:62278142")
+    binding.pry
   end
 
-  def parse_JSON
-
+  def parse_JSON(api_string)
+    parsed_return = JSON.parse(api_string)
   end
   
 end
 
-big = APICommunicator.new("beef") 
+# binding.pry
+big = APICommunicator.new("big")
+parsed = big.parse_JSON(big.get_JSON_file)
+# binding.pry
--- tester.rb
+++ tester.rb
@@ -5,10 +5,10 @@ require 'JSON'
 def import_data
 
  apikey = "0fa7004e9139a09e1423bd884a8947c9:18:62278142" # This isn't going to change.
- query = "'big'" # This is the general search parameter. Put single quotes around it to make it exact.
+ query = "'Big'" # This is the general search parameter. Put single quotes around it to make it exact.
 
  request = {params: {query: query, "api-key" => apikey}}
-
+binding.pry
  # return_value = RestClient.get("http://api.nytimes.com/svc/movies/v2/reviews/search.json?query=big&api-key=0fa7004e9139a09e1423bd884a8947c9:18:62278142")
  # return_value = RestClient.get("http://api.nytimes.com/svc/movies/v2/reviews/search.json?query=big&thousand-best=Y&opening-date=1930-01-01;2000-01-01&api-key=")
  return_string = RestClient.get("http://api.nytimes.com/svc/movies/v2/reviews/search.json", request)
